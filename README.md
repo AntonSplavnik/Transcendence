3d66cedc00472f207d4905146f7d0c05673369a3
Kajus Wurster <kjzl@posteo.de>
2025-11-15 04:12:13 +0100

    minimal testable microservice setup

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Transcendence
 
 A real-time multiplayer web-based pong-game
+
+# Libraries
+
+- https://github.com/yoav0gal/fastify-sqlite-typed
+- https://github.com/fastify/fastify-websocket
+- https://github.com/Bugs5382/fastify-rabbitmq
--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,6 +1,7 @@
-import { join } from 'node:path'
 import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload'
 import { FastifyPluginAsync, FastifyServerOptions } from 'fastify'
+import fastifyRabbit from 'fastify-rabbitmq'
+import { join } from 'node:path'
 
 export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {
 
@@ -13,8 +14,31 @@ const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
   opts
 ): Promise<void> => {
+  const rabbitConnection = process.env.RABBITMQ_URL!;
+  fastify.register(fastifyRabbit, {
+    connection: rabbitConnection
+  });
   // Place here your custom code!
 
+  fastify.ready().then(async () => {
+    fastify.rabbitmq.createConsumer(
+      {
+        queue: 'auth-test-event',
+        queueOptions: { durable: true },
+      },
+      async (msg, _reply) => {
+        const body = msg?.body;
+        const payload =
+          typeof body === 'string'
+            ? body
+            : Buffer.isBuffer(body)
+              ? body.toString('utf8')
+              : JSON.stringify(body);
+
+        fastify.log.info({ payload }, 'received event from rabbit');
+      },
+    );
+  });
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
