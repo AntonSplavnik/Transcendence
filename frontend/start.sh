1950243cc7b9c0ade557518a959e6f251c7c5a70
Drongier <rongier.damien@gmail.com>
2025-12-10 16:00:54 +0100

    Refactor Docker setup for game test and remove unused backend files

---
+++ frontend/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+echo "Game test"
+echo "=================================="
+echo "URL: http://localhost:5173"
+echo "=================================="
+echo ""
+
+npm run dev -- --host 0.0.0.0
